3d8e19e2fb1df8de56c2c3972699df99899a877e
Stanislaw Baranski <stan@stan.bar>
2024-03-16 13:41:33 +0000

    add testnets to hardhat config

--- sc/hardhat.config.ts
+++ sc/hardhat.config.ts
@@ -3,8 +3,32 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import '@typechain/hardhat'
 import '@nomicfoundation/hardhat-ethers'
 
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  defaultNetwork: "alfajores",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0",
+      },
+      chainId: 44787
+    },
+    baseSepolia: {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    },
+    arbitrumSepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614,
+      accounts: [process.env.WALLET_KEY as string],
+    },
+  },
 };
 
 export default config;
