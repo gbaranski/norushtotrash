d7779a4aa12d46f9b1874a41946c28949780c6c4
Gregory Baranski <me@gbaranski.com>
2024-03-16 20:41:25 +0000

    fix: remove unused imporst

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -2,7 +2,6 @@ import type { Eip1193Provider } from 'ethers';
 import type { LayoutLoad } from './$types';
 import { BrowserProvider } from 'ethers';
 import { NoRushToTrash__factory, type NoRushToTrash } from '../../../sc/typechain-types';
-import { PUBLIC_SC_NORUSHTOTRASH_ADDRESS } from '$env/static/public';
 import deployments from '../../../sc/deployments.json';
 
 export const ssr = false;
