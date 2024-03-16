b5270ccfa1b8151c70f837ef93979f54b1944b87
Stanislaw Baranski <stan@stan.bar>
2024-03-16 22:06:36 +0000

    update contract

--- sc/contracts/NoRushToTrash.sol
+++ sc/contracts/NoRushToTrash.sol
@@ -68,9 +68,6 @@ contract NoRushToTrash {
     function reserveListing(uint256 _listingId) external {
         require(!listings[_listingId].isReserved, "Already reserved");
         listings[_listingId].isReserved = true;
-        // use transferFrom instead of transfer
-        // get accept from the sender
-        carbonToken.approve(address(this), rewardAmount);
         carbonToken.transferFrom(msg.sender, address(this), rewardAmount);
         escrows[_listingId] = Escrow(_listingId, msg.sender, false, false);
         emit listingReserved(_listingId, msg.sender);
