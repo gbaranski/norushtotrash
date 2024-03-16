e00adaf8383c6d1d13cdd940c0871460bdc6f89f
Gregory Baranski <me@gbaranski.com>
2024-03-16 12:31:57 +0000

    feat: add support for typechain

--- sc/hardhat.config.ts
+++ sc/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import '@typechain/hardhat'
+import '@nomicfoundation/hardhat-ethers'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
